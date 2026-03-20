271119e950773fdde9aababf3e743c23491e1b04
Pedro Paulo Portella <10197081+pedropauloportella@users.noreply.github.com>
2026-03-20 15:50:45 -0300

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -1,5 +1,5 @@
 export const profile = {
-	fullName: 'Your Name',
+	fullName: 'Pedro Paulo Neves Portella',
 	title: '',
 	institute: '',
 	author_name: '', // Author name to be highlighted in the papers section

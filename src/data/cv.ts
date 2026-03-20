3fc918b88ef00e595217dcef90b1a4a5b9a12c47
Pedro Paulo Portella <10197081+pedropauloportella@users.noreply.github.com>
2026-03-20 19:24:01 -0300

    Update cv.ts

--- src/data/cv.ts
+++ src/data/cv.ts
@@ -2,9 +2,23 @@ export const experiences = [
 	{
 		company: 'Escola Rural São Vicente de Paulo',
 		time: '2022-2023',
-		title: 'Teacher',
-		location: 'Rio de Janeiro, Brasil',
-		description: 'STEM Teacher',
+		title: 'Professor de Cálculo Técnico e Tecnologia',
+		location: 'Rio de Janeiro, RJ, Brasil',
+		description: 'Professor de STEM no ensino fundamental e médio.',
+	},
+	{
+		company: 'ONU-Habitat Brasil',
+		time: '2012-2012',
+		title: 'Agente de Campo',
+		location: 'Rio de Janeiro, RJ, Brasil',
+		description: 'Assistente de pesquisa em um projeto da UPP-Social na favela da Rocinha.',
+	},
+	{
+		company: 'Televisão Cidade S/A',
+		time: '2010-2011',
+		title: 'Analista de Suporte de T.I',
+		location: 'Niterói, RJ, Brasil',
+		description: 'Operação de central de monitoramento e controle de uma rede de telecomunicações.',
 	},
 	// {
 	// 	company: 'Radium Institute (Institut du Radium)',
@@ -17,10 +31,10 @@ export const experiences = [
 
 export const education = [
 	{
-		school: '',
-		time: '',
-		degree: '',
-		location: '',
+		school: 'Universidade Estácio de Sá',
+		time: '2030',
+		degree: 'Bacharel em Engenharia de Computação',
+		location: 'Rio de Janeiro',
 		description: '',
 	},
 	// {
@@ -34,8 +48,8 @@ export const education = [
 
 export const skills = [
 	{
-		title: '',
-		description: '',
+		title: 'Pesquisa e Desenvolvimento',
+		description: 'Auxilia em grupos de pesquisa resolvendo problemas com métodos computacionais',
 	},
 	// {
 	// 	title: 'Experimental Techniques',
@@ -45,12 +59,12 @@ export const skills = [
 
 export const publications = [
 	{
-		title: '',
-		authors: '',
-		journal: '',
-		time: '',
-		link: '',
-		abstract: '',
+		title: 'Cozinhando Um Artigo: Uma Receita De Bolo Para Escrever E Publicar O Seu Primeiro Artigo Científico.',
+		authors: 'Ana Caroline da Silva Vieira, Pedro Paulo Neves Portella',
+		journal: 'Instituto Computacional',
+		time: '2026',
+		link: 'https://books.google.com.br/books/about?id=vSq7EQAAQBAJ',
+		abstract: 'Descubra os fundamentos da escrita científica com este guia. Aprenda a estruturar artigos de excelência, explore métodos rigorosos de revisão bibliográfica e obtenha dicas práticas para planejamento de experimentos e análise crítica de dados. Transforme suas descobertas em relatórios claros para facilitar a validação por pares e a publicação em periódicos. ',
 	},
 	// {
 	// 	title: 'The Radiation of Uranium Compounds',

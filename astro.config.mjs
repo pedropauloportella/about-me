8a8dbef30fbeae5107c28ed43af1006d1e969a70
Pedro Paulo Portella <10197081+pedropauloportella@users.noreply.github.com>
2026-04-23 11:09:43 -0300

    Remove Cloudflare integration from config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,6 @@ import { template } from "./src/settings";
 
 import sitemap from "@astrojs/sitemap";
 
-import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
